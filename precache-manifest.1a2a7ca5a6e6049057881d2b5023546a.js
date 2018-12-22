43061cdd46d40d5b6e050e914516f80bdc81675f
Brian <40767783+brishaw@users.noreply.github.com>
2018-12-22 18:12:42 -0500

    Updates

---
+++ precache-manifest.1a2a7ca5a6e6049057881d2b5023546a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crimson-test-b/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7e285684be1925dfaf0",
+    "url": "/crimson-test-b/static/js/runtime~main.b7e28568.js"
+  },
+  {
+    "revision": "808981d097d86086a761",
+    "url": "/crimson-test-b/static/js/main.808981d0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/crimson-test-b/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "808981d097d86086a761",
+    "url": "/crimson-test-b/static/css/main.549a81a0.chunk.css"
+  },
+  {
+    "revision": "835caddd6d0849ec5145ac87c080c830",
+    "url": "/crimson-test-b/index.html"
+  }
+];
